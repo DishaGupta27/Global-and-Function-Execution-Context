f43ec0f7c60b987653f7d2c3bc99bbb6e6a1e364
DishaGupta27 <guptadisha313@gmail.com>
2025-06-21 22:17:33 +0530

    first

---
+++ EC.js
@@ -0,0 +1,12 @@
+
+let age = 25;
+function displayAge() {
+    console.log("Current age is:", age);
+}
+function changeAge() {
+    age = 30;
+    console.log("Age changed to:", age);
+}
+displayAge();
+changeAge();
+displayAge();  
